950cfe19bf7259a336f76a7e58746b76bf344bf7
Paul Popa <paul.popa@outlook.com>
2023-10-21 23:38:37 +0100

    Create method to make a network request, converting data & publishing a stream of UIImages over time

---
+++ APIFuturesExample/Neworking/PhotoAPIManager.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoAPIManager.swift
+//  APIFuturesExample
+//
+//  Created by Paul on 20/10/2023.
+//
+
+import UIKit
+import SwiftUI
+import Combine
+
+class PhotoAPIManager {
+    
+    private let urlString = "https://picsum.photos/900"
+    private var cancellables = Set<AnyCancellable>()
+    
+    public func fetchUIImage() -> AnyPublisher<UIImage, Error> {
+        
+         guard let url = try? makeURL(from: urlString) else {
+             return Fail(error: NetworkingError.badURL).eraseToAnyPublisher()
+         }
+
+        let publisher = URLSession.shared.dataTaskPublisher(for: url)
+            .mapError { $0 as Error }
+            .map { UIImage(data: $0.data) }
+            .replaceNil(with: UIImage(systemName: "photo.fill")!)
+            .eraseToAnyPublisher()
+        
+        print("Fetching image..")
+        
+        return publisher
+    }
+    
+    public func fetchImage() -> AnyPublisher<Image, Error> {
+        return fetchUIImage()
+            .map { Image(uiImage: $0) }
+            .eraseToAnyPublisher()
+    }
+    
+    private func makeURL(from string: String) throws -> URL {
+        guard let url = URL(string: string) else { throw NetworkingError.badURL }
+        return url
+    }
+}
+
+enum NetworkingError: Error {
+    case badURL
+}
